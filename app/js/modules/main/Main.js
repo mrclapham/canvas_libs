59b21c2e2079c09c5f9cd87e639d80f09872a39c
Graham Clapham <graham.clapham@thomsonreuters.com>
2015-01-20 13:37:20 +0000

    Added the raw templates for the individual pages.

--- app/js/modules/main/Main.js
+++ app/js/modules/main/Main.js
@@ -1,1 +1,21 @@
-var Main = new angular.module('Main', ['ngResource'])
+var Main = new angular.module('Main', ['ngResource', 'ngRoute'])
+Main.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/paper', {
+                templateUrl: './js/modules/main/templates/paper.html',
+                controller: 'PaperController'
+            }).
+            when('/p5', {
+                templateUrl: './js/modules/main/templates/p5.html',
+                controller: 'PaperController'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+    }]);
+
+Main.controller('mainController', ['$scope', function($scope){
+    $scope.data = "mainController data"
+
+}])
--- app/js/modules/main/MainController.js
+++ app/js/modules/main/MainController.js
@@ -1,3 +1,9 @@
 Main.controller('MainController', ['$scope', function($scope){
-    $scope.testalue = "Her is the test value "
+    $scope.testalue = "Here is the test value "
+}])
+
+
+Main.controller('PaperController', ['$scope', function($scope){
+    $scope.testalue = "Here is the test value fom the paper controller "
+    $scope.testalue2 = "Here is the test value fom the paper controller 2"
 }])
